ca65dc9e0211c8744ccd4f72c515db7289657521
Mark Manders <mmjmanders@gmail.com>
2026-02-18 14:34:19 +0100

    Feature/add dependencies (#1)
    * Add ESLint configuration, Husky hooks, and workspace settings
    
    * Integrate Tailwind CSS with Vite and configure global styles
    
    * Add favicon and update Nuxt app head configuration
    
    * Add @nuxt/fonts module, update favicon, and apply custom font styling
    
    * Add @vite-pwa/nuxt module to enhance PWA support
    
    * Enhance PWA setup with manifest configuration, icons, and auto-update support
    
    * Update global font styles with Manrope and Petrona integration
    
    * Remove favicon file
    
    * Update @nuxt/fonts to v0.14.0 and add pnpm package manager configuration
    
    * Add GitHub Actions workflow for linting and `lint` script to package.json
    
    * Added typescript to devDependencies to fix eslint
    
    * Move @nuxt/eslint to dependencies in package.json
    
    * Add GitHub Actions workflow for deploying to GitHub Pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,60 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@vite-pwa/nuxt'],
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: 'Escape Room',
+      link: [
+        { rel: 'icon', href: '/favicon.svg', type: 'image/svg+xml' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon-180x180.png' },
+      ],
+    },
+  },
+  css: ['~/assets/css/main.css'],
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  pwa: {
+    devOptions: {
+      enabled: true,
+    },
+    strategies: 'generateSW',
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Escape Room',
+      short_name: 'Escape Room',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable',
+        },
+      ],
+    },
+  },
 })
