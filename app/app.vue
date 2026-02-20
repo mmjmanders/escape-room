a2386ebf089e5e34762935e32a4ff3d3a8f6ecd9
Mark Manders <mmjmanders@gmail.com>
2026-02-20 11:48:59 +0100

    Refactor Nuxt app head configuration: move title and meta description to `app.vue`

--- app/app.vue
+++ app/app.vue
@@ -3,9 +3,13 @@ import favicon from '~/assets/favicon.svg'
 import appleTouchIcon from '~/assets/apple-touch-icon-180x180.png'
 
 useHead({
+  title: 'Escape Room',
   link: [
     { rel: 'icon', href: favicon, type: 'image/svg+xml' },
     { rel: 'apple-touch-icon', href: appleTouchIcon },
+  ],
+  meta: [
+    { name: 'description', content: 'Escape Room' },
   ],
 })
 </script>
