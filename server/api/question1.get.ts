7edd62b1c61ea35a57743c30202316507c2d0f6d
Mark Manders <mmjmanders@gmail.com>
2026-02-22 23:36:59 +0100

    Fix: remove unused parameter in `question1.get` handler

--- server/api/question1.get.ts
+++ server/api/question1.get.ts
@@ -1,6 +1,6 @@
 import type { Question } from '#shared/schemas/question'
 
-export default defineEventHandler(async (_) => {
+export default defineEventHandler(async () => {
   const question: Question = {
     question: 'In welk jaar stond Oranje voor het eerst in de finale van een WK voetbal?',
   }
