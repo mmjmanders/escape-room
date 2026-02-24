fdf832a176f3b5390ad9ca7342195924da483fbd
Mark Manders <mmjmanders@gmail.com>
2026-02-24 15:10:32 +0100

    Update Intro component and global styles for improved visuals and typography (#14)
    - Add warning icon to "Let op" section in `Intro.vue` for better emphasis.
    - Replace global font family with Roboto and adjust heading styles for increased visual consistency.

--- app/components/Intro.vue
+++ app/components/Intro.vue
@@ -17,7 +17,9 @@ useHead({
       <p>De escape room bestaat uit 5 vragen over je lievelingsartiest JOB.</p>
       <p>Als je alle 5 de vragen goed hebt, krijg je een beloning.</p>
       <p class="text-lg mt-2">
-        <span class="font-semibold">Let op:</span> als je een vraag fout hebt, moet je 5 seconden wachten om het opnieuw te proberen. De totale tijd is maximaal 10 minuten.
+        <span class="font-semibold">
+          <Icon name="material-symbols:warning-outline-rounded" />
+          Let op:</span> als je een vraag fout hebt, moet je 5 seconden wachten om het opnieuw te proberen. De totale tijd is maximaal 10 minuten.
       </p>
     </div>
     <button
